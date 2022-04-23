4a4d293d77b8f52e812df3c6008b082b01c3287a
Danilo Santos <danilo.aparecido.santos@gmail.com>
2022-04-23 05:41:36 -0300

    fazendo verificação do nginx instalado

--- teste/verifica_ambiente.sh
+++ teste/verifica_ambiente.sh
@@ -2,17 +2,15 @@
 
 cd ../contruindo_ambiente/0-terraform
 
-echo "Aguardando criação de maquinas ..."
-sleep 10 # 10 segundos
-
-PUBLIC_DNS=$(terraform output public_dns)
+PUBLIC_DNS="$(terraform output | grep public_dns | awk '{print $2;exit}')"
 PUBLIC_DNS=$(echo $PUBLIC_DNS | sed -e "s/\"//g")
+PUBLIC_DNS=$(echo $PUBLIC_DNS | sed -e "s/,//g")
 
 retorno=`curl http://$PUBLIC_DNS`
 
 if [[ "$retorno" =~ "Welcome to nginx!" ]]
 then
-  echo "Processo instalado com sucesso"
+  echo "Verificação de teste com sucesso"
   exit 0
 else
   echo "Erro na instalação"
